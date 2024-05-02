bc1201ab26595c1373ab81258fd0bcb7ad8a2202
Chouaibmoumnii <chouaib.moumni@esprit.tn>
2024-05-02 01:21:27 +0100

    first commit

--- prediction.py
+++ prediction.py
@@ -16,7 +16,7 @@ results_collection = db['results']
 matches_collection = db['matches']
 
 def collect_match_results():
-    # Initialize dictionaries for historical and upcomings matches
+    # Initialize dictionaries for historical and upcomingss matches
     historical_matches = {}
     upcoming_matches = {}
 
